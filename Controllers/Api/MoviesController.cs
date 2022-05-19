0f094192af60dcc501b17c8336465e85a55308d5
akshat.upadhyay <akshat.upadhyay@outlook.com>
2022-05-20 03:59:58 +0530

    Added Movie API controller.

---
+++ Controllers/Api/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+using Vidly.Data;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext injectedContext)
+        {
+            _context = injectedContext;
+        }
+
+        // GET /api/movies
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            var movies = _context.Movies.ToList();
+            return Ok(movies);
+        }
+
+        // GET /api/movies/1
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(x => x.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+        // POST /api/movies
+        [HttpPost]
+        public IActionResult CreateMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            movie.DateAdded = DateTime.Now;
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.GetDisplayUrl()), movie);
+        }
+
+        // PUT /api/movies/1
+        [HttpPut("{id}")]
+        public IActionResult UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = _context.Movies.SingleOrDefault(movie => movie.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            movieInDb.Name = movie.Name;
+            movieInDb.ReleaseDate = movie.ReleaseDate;
+            movieInDb.NumberInStock = movie.NumberInStock;
+            movieInDb.GenreId = movie.GenreId;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE /api/movies/1
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(movie =>movie.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Remove(movieInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
